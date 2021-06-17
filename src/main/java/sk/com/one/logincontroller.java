0a2db7892b388660e9e5a44a888d9354c60688c7
skgit <1450790367@qq.com>
2021-06-17 09:06:02 +0800

    header

---
+++ src/main/java/sk/com/one/logincontroller.java
@@ -0,0 +1,15 @@
+package sk.com.one;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping( "/user")
+public class logincontroller {
+	
+	 @RequestMapping( "/index")
+	 public String index() {
+		 		System.out.println( "user index. . ." );
+		 		return "index";
+	 }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+	<a href="/sk/user/index.do">点击刷新</a>
+</body>
+</html>
